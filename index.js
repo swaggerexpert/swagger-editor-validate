d8713d206756e6492c7e1e657881a87fc617c01e
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-03-06 02:06:45 +0100

    commit

--- index.js
+++ index.js
@@ -3,15 +3,37 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false, slowMo: 2000 });
     const page = await browser.newPage();
     await page.goto('http://localhost/');
     await page.evaluate(() => {
        localStorage.setItem('swagger-editor-content', 'test');
     });
-    await page.goto('http://localhost/');
-    const title = await page.title();
-    console.dir(title);
+    await page.reload();
+
+    // no API definition provided
+    const noApiDefinition = await page.evaluate(() => {
+       const element = document.querySelector('.swagger-ui .loading-container h4');
+       console.dir(element);
+       if (element === null) {
+           return false;
+       }
+       return element.innerHTML === 'No API definition provided.';
+    });
+    if (noApiDefinition) {
+        console.error('No API definition provided.')
+        process.exit(1);
+    }
+
+    // unable to render definition
+    const unableToRenderDefinition = await page.evaluate(() => {
+       const element = document.querySelector('.swagger-ui .version-pragma');
+       return element !== null;
+    });
+    if (unableToRenderDefinition) {
+        console.error('Unable to render this definition.')
+        process.exit(1)
+    }
 
     await browser.close();
 })();
