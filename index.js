0deb2a06113e2b8baa4375e791460d0f14355886
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-03-06 01:24:42 +0100

    commit

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost/');
-
+    await page.evaluate(() => {
+       localStorage.setItem('swagger-editor-content', 'test');
+    });
+    await page.goto('http://localhost/');
     const title = await page.title();
     console.dir(title);
 
