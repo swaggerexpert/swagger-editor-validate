975c8821ccde97b3ec65f3a754f85bde7e4062e2
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-03-06 01:17:56 +0100

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:8080/');
+    await page.goto('http://localhost/');
 
     const title = await page.title();
     console.dir(title);
