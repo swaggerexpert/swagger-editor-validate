67b01b32fbc059e3a65c355cccfebcab89ec22d4
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-08-09 14:52:26 +0200

    chore(deps-dev): bump prettier from 2.8.8 to 3.0.1
    Refs #378

--- examples/ignore-error.js
+++ examples/ignore-error.js
@@ -1,11 +1,15 @@
 const shouldIgnoreError = (error) => {
   if (error.lineNo === 2) {
-    if (/should NOT have additional properties\s*additionalProperty: contact1/mg.test(error.message)) {
+    if (
+      /should NOT have additional properties\s*additionalProperty: contact1/gm.test(
+        error.message
+      )
+    ) {
       return true;
     }
   }
 
   return false;
-}
+};
 
 module.exports = shouldIgnoreError;
